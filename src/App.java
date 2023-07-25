797feb14aea2d938eea6ab74bae6189c8eed3390
Aiker Ary Acosta Cantillo <aikiringo26@gmail.com>
2023-07-25 18:37:42 -0500

    Trying some new focus to the AskerAndReader class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Create a new asker and reader
+        AskerAndReader askerAndReader = new AskerAndReader();
+        // Use this to ask the user for their name
+        String name = (String) askerAndReader.ask("What is your name? ");
+        // Use this to print the user's name
+        askerAndReader.print("Hello, " + name + "!");
+        // Use this to ask the user for their age as an integer
+        int age = (int) askerAndReader.ask("What is your age? ");
+        // Use this to print the user's age
+        askerAndReader.print("You are " + age + " years old.");
     }
 }
--- src/AskerAndReader.java
+++ src/AskerAndReader.java
@@ -1,81 +1,31 @@
 import java.util.Scanner;
 
 /**
- * A utility class for reading input from the user and printing output to the console.
+ * A utility class for reading input from the user and printing output to the
+ * console.
  */
 public class AskerAndReader {
 
     private static Scanner scanner = new Scanner(System.in);
 
     /**
-     * Prompts the user with the given question and reads an integer from the console.
-     * @param question the question to ask the user
-     * @return the integer entered by the user
+     * The function asks a question and returns the user's input as an Object.
+     * 
+     * @param question A string representing the question that will
+     *                 be displayed to the user.
+     * @return The method is returning an Object.
      */
-    public int askInt(String question) {
+    public Object ask(String question) {
         System.out.print(question);
-        return scanner.nextInt();
+        return scanner.nextLine();
     }
 
     /**
-     * Prompts the user with the given question and reads a float from the console.
-     * @param question the question to ask the user
-     * @return the float entered by the user
+     * Prints the given object to the console.
+     * 
+     * @param message the object to print
      */
-    public float askFloat(String question) {
-        System.out.print(question);
-        return scanner.nextFloat();
-    }
-
-    /**
-     * Prompts the user with the given question and reads a string from the console.
-     * @param question the question to ask the user
-     * @return the string entered by the user
-     */
-    public String askString(String question) {
-        System.out.print(question);
-        return scanner.next();
-    }
-
-    /**
-     * Prompts the user with the given question and reads a boolean from the console.
-     * @param question the question to ask the user
-     * @return the boolean entered by the user
-     */
-    public boolean askBoolean(String question) {
-        System.out.print(question);
-        return scanner.nextBoolean();
-    }
-
-    /**
-     * Prints the given message to the console.
-     * @param message the message to print
-     */
-    public void print(String message) {
-        System.out.println(message);
-    }
-
-    /**
-     * Prints the given integer to the console.
-     * @param message the integer to print
-     */
-    public void print(int message) {
-        System.out.println(message);
-    }
-
-    /**
-     * Prints the given float to the console.
-     * @param message the float to print
-     */
-    public void print(float message) {
-        System.out.println(message);
-    }
-
-    /**
-     * Prints the given boolean to the console.
-     * @param message the boolean to print
-     */
-    public void print(boolean message) {
-        System.out.println(message);
+    public void print(Object message) {
+        System.out.println(message.toString());
     }
 }
